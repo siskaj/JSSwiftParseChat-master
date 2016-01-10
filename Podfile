65ce37d681cfdc6c016d08ba7abee1561f8e877b
Jaromir Siska <siskaj@me.com>
2016-01-10 16:14:40 +0100

    Initial commit.

---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.2'
+use_frameworks!
+
+pod 'Alamofire'
+pod 'JSQMessagesViewController'
+pod 'Parse'
+pod 'ParseUI'
+pod 'ParseFacebookUtilsV4'
+pod 'FBAudienceNetwork'
+pod 'ParseCrashReporting'
+pod 'SVProgressHUD'
---
+++ SwiftParseChat/SwiftParseChat-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftParseChat-Bridging-Header.h
+//  SwiftParseChat
+//
+//  Created by Jesse Hu on 2/20/15.
+//  Copyright (c) 2015 Jesse Hu. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <ParseFacebookUtilsV4/ParseFacebookUtilsV4.h>
+#import <ProgressHUD/ProgressHUD.h>
+#import <JSQMessages.h>
+#import <APAddressBook-Bridging.h>
